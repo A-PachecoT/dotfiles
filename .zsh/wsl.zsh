a0b503db5a0f1e4a508d93563eefd49bc157e6fa
A-PachecoT <andre.pacheco.t@uni.pe>
2024-12-03 10:22:57 -0500

    feat: viagrant config

--- .zsh/wsl.zsh
+++ .zsh/wsl.zsh
@@ -12,6 +12,11 @@ alias cu='cursor .'
 alias ex='explorer.exe .'
 
 # YaVendio utils
-alias yavgatestg='cd ~/yav/message-gateway && git pull --rebase && vim config/stg.yaml'
-alias yavgateprd='cd ~/yav/message-gateway && git pull --rebase && vim config/prd.yaml'
+alias yavgatestg='cd ~/yav/message-gateway && git checkout main && git pull --rebase && vim config/stg.yaml'
+alias yavgateprd='cd ~/yav/message-gateway && git checkout main && git pull --rebase && vim config/prd.yaml'
 alias cdya='cd /mnt/d/yavendio/clients_backends' 
+
+# Setup Vagrant variables
+export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/Users/Andre/"
+export PATH="$PATH:/mnt/c/Program Files/Oracle/VirtualBox"
