043bfa66e154f3ebfca30ce99837a2b83174904a
A-PachecoT <andre.pacheco.t@uni.pe>
2025-02-27 19:04:24 -0500

    Add Go bin directory to PATH

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -5,6 +5,7 @@ export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=1
 
 # Path configurations
 export PATH="$HOME/bin:$PATH"
+export PATH=$PATH:$HOME/go/bin
 
 # Source additional environment variables
 . "$HOME/.local/bin/env" 
