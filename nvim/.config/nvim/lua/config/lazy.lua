c9a51494146b66194b44237b96ef97118bcd602b
A-PachecoT <andre.pacheco.t@uni.pe>
2026-01-27 14:08:30 -0500

    fix(nvim): disable lazy.nvim update notifications on startup

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
     version = false,
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
   performance = {
     rtp = {
       disabled_plugins = {
